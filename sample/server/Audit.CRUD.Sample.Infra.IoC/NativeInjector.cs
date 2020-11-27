74c9425966fbfc2f4c0d1ac2d16013118e06d066
Camila Machado <camila_melomachado@hotmail.com>
2020-11-27 16:13:37 -0300

    feat (sample): Configurando projeto de injecção de dependencia

---
+++ sample/server/Audit.CRUD.Sample.Infra.IoC/NativeInjector.cs
@@ -0,0 +1,17 @@
+﻿using Audit.CRUD.Sample.Domain.Features.Users;
+using Audit.CRUD.Sample.Infra.Data.Context;
+using Audit.CRUD.Sample.Infra.Data.Features.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Audit.CRUD.Sample.Infra.IoC
+{
+    public static class NativeInjector
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Infra - Data
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<AuditCRUDSampleDbContext>();
+        }
+    }
+}
