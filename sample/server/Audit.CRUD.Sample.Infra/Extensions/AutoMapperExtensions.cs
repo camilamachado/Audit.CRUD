94f4283f2ade55aebc0ee75079d99f35c165db37
Camila Machado <camila_melomachado@hotmail.com>
2020-11-27 16:12:05 -0300

    feat (sample): Configurando auto mapper e swagger

---
+++ sample/server/Audit.CRUD.Sample.Infra/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+
+namespace Audit.CRUD.Sample.Infra.Extensions
+{
+    public static class AutoMapperExtensions
+    {
+        public static void ConfigureProfiles(this object any, params Type[] types)
+        {
+            Mapper.Reset();
+            Mapper.Initialize(cfg =>
+            {
+                foreach (Type type in types)
+                {
+                    cfg.AddProfiles(type);
+                }
+            });
+        }
+    }
+}
