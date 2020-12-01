ff7db9b8738f00e028ecd393d94c07b95015d595
Camila Machado <camila_melomachado@hotmail.com>
2020-12-01 01:27:47 -0300

    feat (Audit.CRUD): Criando service e repository responsáveis por lógica de persistencia de dados

---
+++ src/Audit.CRUD/Application/AuditCRUDService.cs
@@ -0,0 +1,50 @@
+﻿using Audit.CRUD.Common.Structs;
+using Audit.CRUD.Domain;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Audit.CRUD.Application
+{
+	public class AuditCRUDService : IAuditCRUD
+	{
+		private readonly IAuditLogRepository _auditRepository;
+
+		public AuditCRUDService(IAuditLogRepository auditRepository)
+		{
+			_auditRepository = auditRepository;
+		}
+
+		public async Task<Result<Exception, Unit>> Create(UserAuditCRUD user, string eventName, object currentEntity, string location, string ipAddress, string reason)
+		{
+			var auditLog = new AuditLog();
+			auditLog.Timestamp = DateTime.Now;
+			auditLog.User = user;
+			auditLog.EventName = eventName;
+			auditLog.CurrentEntity = JsonConvert.SerializeObject(currentEntity); ;
+			auditLog.Location = location;
+			auditLog.IpAddress = ipAddress;
+			auditLog.Reason = reason;
+			auditLog.Action = Actions.Create;
+
+			var addCb = await _auditRepository.AddAsync(auditLog);
+
+			return Unit.Successful;
+		}
+
+		public Task<Result<Exception, Unit>> Delete()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<Exception, Unit>> Read()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Result<Exception, Unit>> Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Audit.CRUD/Repository/AuditLogRepository.cs
@@ -0,0 +1,33 @@
+﻿using Audit.CRUD.Common.Structs;
+using Audit.CRUD.Domain;
+using Audit.CRUD.Repository.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace Audit.CRUD.Repository
+{
+	public class AuditLogRepository : IAuditLogRepository
+	{
+		private readonly AuditCRUDDbContext _context;
+
+		public AuditLogRepository(AuditCRUDDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Result<Exception, Unit>> AddAsync(AuditLog auditLog)
+		{
+			var indexResponse = await _context.Database.IndexDocumentAsync(auditLog);
+
+			if (!indexResponse.IsValid)
+			{
+				var debugInformation = indexResponse.DebugInformation;
+				var exception = indexResponse.OriginalException;
+
+				return exception;
+			}
+
+			return Unit.Successful;
+		}
+	}
+}
